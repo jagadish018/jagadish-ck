65fd661cfa0878c788342fa6c66ac1502e542fb3
jagadishck018 <jagadishck.018@gmail.com>
2025-04-29 09:18:32 +0530

    Refactor ContactBlock form to include action and method attributes; add hidden inputs for captcha and redirect URL.

--- app/block/contact-block/ContactBlock.tsx
+++ app/block/contact-block/ContactBlock.tsx
@@ -13,6 +13,8 @@ const ContactBlock = () => {
 
       <main className="flex-grow flex justify-center items-start pt-10 px-4">
         <motion.form
+          action="https://formsubmit.co/6a794e02b6e916d45ad18dfa735b15c4" // FormSubmit.co action
+          method="POST"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
@@ -23,23 +25,39 @@ const ContactBlock = () => {
           <h2 className="text-white font-semibold text-center mb-4">
             Get In Touch
           </h2>
+
+          <input type="hidden" name="_captcha" value="false" />
+          <input
+            type="hidden"
+            name="_next"
+            value="https://your-website.com/thank-you" // Optional: redirect after form submit
+          />
+
           <input
             type="text"
+            name="name"
+            required
             placeholder="Name"
             className="bg-[#464a4f] text-white rounded-lg py-3 px-4 placeholder-white focus:outline-none"
           />
           <input
             type="email"
+            name="email"
+            required
             placeholder="Email"
             className="bg-[#464a4f] text-white rounded-lg py-3 px-4 placeholder-white focus:outline-none"
           />
           <input
             type="text"
+            name="subject"
+            required
             placeholder="Subject"
             className="bg-[#464a4f] text-white rounded-lg py-3 px-4 placeholder-white focus:outline-none"
           />
           <textarea
             rows={4}
+            name="message"
+            required
             placeholder="Message"
             className="bg-[#464a4f] text-white rounded-lg py-3 px-4 placeholder-white resize-none focus:outline-none"
           ></textarea>
